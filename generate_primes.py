fbc57716893d8aa1c5e47065a692fa07042858d9
esirK <esirkings@gmail.com>
2017-04-04 08:01:06 +0300

    Update on primes_generator on how the numbers are generated

--- generate_primes.py
+++ generate_primes.py
@@ -1,7 +1,6 @@
 import numbers
 import decimal
 def primes_generator(n):
-<<<<<<< HEAD
     primes=[]
     ikondaani=[]
     if type(n)==int:
@@ -10,14 +9,6 @@ def primes_generator(n):
             for x in range(2,n):
                 if x not in noprimes:
                     primes.append(x)
-=======
-    primes=[2]
-    if type(n)==int:
-        if n>1:                
-            for i in range(2,n+1):           
-                if i%2==1:
-                    primes.append(i)
->>>>>>> 7bcbb199e3131fbf7e59600ccb28e68f67787786
             return primes
         else:
             return "Not A Prime Number"
@@ -26,9 +17,4 @@ def primes_generator(n):
     
     elif type(n)==list:
         return 'Not A prime Number'
-<<<<<<< HEAD
 #print primes_generator(300)
-=======
-
-#print primes_generator(19.9)
->>>>>>> 7bcbb199e3131fbf7e59600ccb28e68f67787786
