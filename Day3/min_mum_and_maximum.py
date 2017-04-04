be1ee4101192bb5e0990bb9bd6284931eb40800e
esirK <esirkings@gmail.com>
2017-04-05 00:42:53 +0300

    Added the find_ma_min function To The Day Three Boot Camp Work

---
+++ Day3/min_mum_and_maximum.py
@@ -0,0 +1,17 @@
+def find_max_min(L):
+    #create a Set To remove duplicates
+    set_of_numbers=set(L)
+
+    #Change them To A List To Enable sort operation on The List
+    newList=list(set_of_numbers)
+
+    #sort the Numbers
+    
+    sortedList=sorted(newList)
+    minimum=sortedList[0]
+    maximum=sortedList[-1]
+    if minimum==maximum:
+        return [len(L)]
+    else:
+        return[minimum,maximum]
+#print find_max_min([4, 66, 6, 44, 7, 78, 8, 68, 2])
