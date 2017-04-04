2d3a7327ec77528259fca74847e0ecefc06c3749
esirK <esirkings@gmail.com>
2017-04-04 04:43:23 +0300

    Added 4 More  test Case For Deternimeing if n is a Number and is  non negative and greater than 2(minimum prime number)

--- prime_nums_generate_tests.py
+++ prime_nums_generate_tests.py
@@ -4,5 +4,19 @@ from generate_primes import primes_generator
 class TestPrimeNumbers(unittest.TestCase):
     def test_a_prime(self):
         self.assertEqual(primes_generator(5),[2,3,5])
+
+    def test_not_stings(self):
+        self.assertEqual(primes_generator('five'),'Not A Prime Number')
+
+    def test_not_negative(self):
+        self.assertEqual(primes_generator(-5),'A is Non Negative')
+
+    def test_minimum_prime_number(self):
+        self.assertEqual(primes_generator(1),'Not A Prime Number')
+
+    def test_not_list(self):
+        self.assertEqual(primes_generator([2]),'Not A prime Number')
+
+    
 if __name__=='__main__':
     unittest.main()
