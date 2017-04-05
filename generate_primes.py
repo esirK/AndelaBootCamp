5ea383ebe688d75e5da2d38ae8dd4b350f706022
esirK <esirkings@gmail.com>
2017-04-05 22:51:45 +0300

    Removed Unneeded imports

--- generate_primes.py
+++ generate_primes.py
@@ -1,8 +1,5 @@
-import numbers
-import decimal
 def primes_generator(n):
     primes=[]
-    #ikondaani=[]
     if type(n)==int:
         if n>1:                
             noprimes = set(j for i in range(2, n+1) for j in range(i*2, n+1, i))
@@ -17,4 +14,4 @@ def primes_generator(n):
     
     elif type(n)==list:
         return 'Not A prime Number'
-#print primes_generator(5)
+
