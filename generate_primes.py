0c4edd1783c38406fb9f737703d342afe81ae18a
esirK <esirkings@gmail.com>
2017-04-05 23:02:29 +0300

    Fixed Syntax Error when generating the primes

--- generate_primes.py
+++ generate_primes.py
@@ -6,7 +6,7 @@ def primes_generator(n):
             for x in range(2,n+1):
                 if x not in noprimes:
                     primes.append(x)
-            return primes
+            print primes
         else:
             return "Not A Prime Number"
     elif type(n)==str:
@@ -14,4 +14,4 @@ def primes_generator(n):
     
     elif type(n)==list:
         return 'Not A prime Number'
-
+#primes_generator(60)
