9b537b334c9ff31afe959ba17c0cd2fb1283449c
esirK <esirkings@gmail.com>
2017-04-05 11:19:04 +0300

    Allowed output Dictionary to include numbers

--- Day3/word_count.py
+++ Day3/word_count.py
@@ -4,5 +4,11 @@ def words(A):#A String
     for word in split_words:
         list_of_words.append(word)
 
-    count_words={i:list_of_words.count(i) for i in list_of_words}
+    lis={i:list_of_words.count(i) for i in list_of_words if not i.isdigit()}
+    #{'testing': 2, 1: 1, 2: 1}
+    count_words={int(k):list_of_words.count(k) for k in list_of_words if k.isdigit()}
+    count_words.update(lis)
     return count_words
+
+#print words("testing 1 2 testing")
+#print words('car : carpet as java : javascript!!&@$%^&')
