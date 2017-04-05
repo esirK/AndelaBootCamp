81cd438edf0ea482d41dd09b58041fe47b4be912
esirK <esirkings@gmail.com>
2017-04-05 22:44:11 +0300

    Real world OOP redone using Car Object

--- Day2/real_world_oop.py
+++ Day2/real_world_oop.py
@@ -1,61 +1,63 @@
-class Employee(object):
-    raise_saraly_by=1.04
-    numberOfEmployee=0
-    def __init__(self,firstName,lastName,pay):
-        self.firstName=firstName
-        self.lastName=lastName
-        self.pay=pay
-        self.email=firstName+'.'+lastName+'@company.com'
-        Employee.numberOfEmployee+=1
+class Car(object):
+    car_ratting=1.04
+    number_of_wheels=4
+    numberOfCars=0
+    def __init__(self,carName,carType,price):
+        self.carName=carName
+        self.carType=carType
+        self.price=price
+        self.description=str(carName)+'.'+str(carType)+' Costing '+str(price)
+        Car.numberOfCars+=1
 
-    def displayNumberOfEmployees(self):
-        print "Total Employees %d"%Employee.numberOfEmployee
+    def displayTotalNumberOfCars(self):
+        print "Total Cars %d"%Car.numberOfCars
         
-    def fullname(self):
-        #return '{} {}'.format(self.firstName,self.lastName)
-        return self.firstName+' '+self.lastName
-    def apply_raise(self):
-        self.pay=int(self.pay*self.raise_amt)
+    def car_name_and_model(self):
+        return self.carName+' '+self.carType
+    def raise_rateing(self,raise_amt):
+        self.price=int(self.price*self.raise_amt)
 
-class Developer(Employee):#Inheritance
-    raise_saraly_by=1.07#Information Hideing
-    def __init__(self,firstName,lastName,pay,programmingLanguage):
-        super(Developer,self).__init__(firstName,lastName,pay)
-        self.programmingLanguage=programmingLanguage
+class Truck(Car):#Inheritance
+    number_of_wheels=6#Information Hideing
+    def __init__(self,carName,carType,price,weight):
+        super(Truck,self).__init__(carName,carType,price)
+        self.weight=weight
 
-class Manager(Employee):
-    def __init__(self,firstName,lastName,pay,employees=None):
-        super(Manager,self).__init__(firstName,lastName,pay)
-        if employees is None:
-            self.employees=[]
+class Matatu(Car):
+    def __init__(self,carName,carType,price,number_of_passagers,places_of_operation):
+        super(Matatu,self).__init__(carName,carType,price)
+        self.number_of_passagers=number_of_passagers
+        if places_of_operation is None:
+            self.places_of_operation=[]
         else:
-            self.employees=employees
+            self.places_of_operation=places_of_operation
 
-    def add_employees(self,employee):
-        if employee not in self.employees:
-            self.employees.append(employee)
-    def remove_employee(self,employee):
-        if employee in self.employees:
-            self.employees.remove(employee)
-    def employees_managed(self):
-        for emp in self.employees:
-            print ('-->',emp.firstName+' '+emp.lastName)
+    def add_place_of_operation(self,place_of_operation):
+        if place_of_operation not in self.places_of_operation:
+            self.places_of_operation.append(place_of_operation)
+    def remove_place_of_operation(self,place_of_operation):
+        if place_of_operation in self.places_of_operation:
+            self.places_of_operation.remove(place_of_operation)
+    def routes_of_operation(self):
+        routes=[]
+        for route in self.places_of_operation:
+            routes.append(route)
+        return routes
+    
 
-emp_1=Employee('Esir','Kings',35000)
-emp_2=Employee('Sarah','Wangari',25000)
-developer_1=Developer('Mwakai','Wakesho',35000,'C++')
+car_1=Car('Toyota','Colora',"KSH 350,000")
+car_2=Car('Pigeot','max',"KSH 725,000")
+truck_1=Truck('Izuzu','Dmax',"KSH 3,500,000",'5 Tones')
 
-manager_1=Manager('Kimoda','Fransis',70000,[emp_1])
-manager_1.add_employees(developer_1)
-manager_1.remove_employee(emp_1)
-
+matatu_1=Matatu('Nissan','Box',"700,000","7 Seater",["Nyahururu"])
+matatu_1.add_place_of_operation("Nairobi")
 
 
-print  developer_1.fullname()+' Programming In  '+developer_1.programmingLanguage#A developer And His/her Prog Language
 
-print manager_1.fullname()+' Manager '
+print car_1.car_name_and_model()+' '
+print truck_1.car_name_and_model()+" Weighn "+truck_1.weight
 
-print manager_1.employees_managed()#Employees Managed By This Manager
+print matatu_1.car_name_and_model()+" Carraying "+matatu_1.number_of_passagers+" Operating Withing "+str(matatu_1.routes_of_operation())
 
-manager_1.displayNumberOfEmployees()
+truck_1.displayTotalNumberOfCars()
 
