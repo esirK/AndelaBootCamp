a1b7ba69476719fde2b78a146f32f3c66afa1603
esirK <esirkings@gmail.com>
2017-04-06 08:43:41 +0300

    Added the missing_number function

---
+++ Day4/missing_number.py
@@ -0,0 +1,29 @@
+def find_missing(A,B):
+    #check if contents of list are numbers
+    AandB=A+B
+    if all(isinstance(item,int) for item in AandB):    
+        set1=set(A)
+        set2=set(B)
+
+        d_set1_and_set2=len(set1-set2)
+        d_set2_and_set1=len(set2-set1)
+
+        
+        if (d_set1_and_set2)>1 or (d_set2_and_set1)>1 or (d_set2_and_set1+d_set1_and_set2)>1:
+            print "The arrays should be diffrent by only one extra number"
+            #return "The arrays should be diffrent by only one extra number"
+        else:
+            if d_set1_and_set2>0:
+                print list(set1-set2)[0]
+                #return list(set1-set2)[0]
+            elif (d_set1_and_set2==0)and(d_set2_and_set1==0):
+                print "0"
+                #return 0
+            else:
+                print list(set2-set1)[0]
+                #return list(set2-set1)[0]
+    
+    else:
+        print "The Arrays Should Only Contain Numbers"
+        #return "The Arrays Should Only Contain Numbers"
+#find_missing([], [])
