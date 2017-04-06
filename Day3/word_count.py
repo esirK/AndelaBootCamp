1a9fc913100c5fc8b7f892cf54f8cb1256a422c9
esirK <esirkings@gmail.com>
2017-04-06 18:05:22 +0300

    Update word_count.py

--- Day3/word_count.py
+++ Day3/word_count.py
@@ -8,7 +8,8 @@ def words(A):#A String
     #{'testing': 2, 1: 1, 2: 1}
     count_words={int(k):list_of_words.count(k) for k in list_of_words if k.isdigit()}
     count_words.update(lis)
-    return count_words
+    #return count_words
+    print count_words
 
 #print words("testing 1 2 testing")
 #print words('car : carpet as java : javascript!!&@$%^&')
